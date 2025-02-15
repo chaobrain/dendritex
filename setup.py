62b6c70112958856750e421e0335c768481b94bf
chaoming <adaduo@outlook.com>
2025-02-16 01:02:47 +0800

    update version

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
